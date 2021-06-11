ec34f673f96af0c79c8269736c0709b2ed78cd77
J_Kiser <jdkiser228@gmail.com>
2021-06-11 15:53:21 -0400

    developing home-routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,17 +1,36 @@
 const router = require('express').Router();
-const { Post, User } = require('../models');
+const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
     try {
       // Get all projects and JOIN with user data
       const postData = await Post.findAll({
-        include: [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-        ],
+            attributes: [
+              'id',
+              'title',
+              'post_content',
+              'data_created'
+            ],
+            include: [{
+              model: Comment,
+              attributes: [
+                'id',
+                'comment_content',
+                'post_id',
+                'user_id',
+                'date_created'
+              ],
+              include: {
+                model: User,
+                attributes: ['username']
+              }
+            },
+            {
+              model: User,
+              attributes: ['username']
+            }
+          ]
       });
   
       // Serialize data so the template can read it
@@ -26,4 +45,18 @@ router.get('/', async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  router.get('/login', (req, res) => {
+    if(req.session.loggin_in) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
+  });
+
+  router.get('/signup', (req, res) => {
+    res.render('signup');
+  });
+
+  module.exports = router;
     
