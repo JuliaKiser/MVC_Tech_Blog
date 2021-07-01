00f96e24e836780be5967d98ccb86a771783d5c1
J_Kiser <jdkiser228@gmail.com>
2021-07-01 12:44:38 -0400

    updating user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -10,3 +10,49 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: {exclude: '[password]'},
+        where: {
+            id: req.params.id
+        },
+        include: [{
+            model: Post,
+            attributes: [
+                'id',
+                'title',
+                'post_content',
+                'date_created'
+            ]
+        },
+        {
+            model: Comment,
+            attributes: [
+                'id',
+                'comment_content',
+                'date_created'
+            ],
+            include: {
+                model: Post,
+                attributes: ['title', 'date_created']
+            }
+        },
+        {
+            model: Post,
+            attributes: ['title', 'date_created'],
+        }
+    ]
+    })
+    .then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({message: 'No user found'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
