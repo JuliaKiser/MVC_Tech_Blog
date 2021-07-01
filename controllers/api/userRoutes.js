ad4d8e7148b5cba9a972a60250a9d7e50dcd3725
J_Kiser <jdkiser228@gmail.com>
2021-07-01 12:52:24 -0400

    userroutes update

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -56,3 +56,47 @@ router.get('/:id', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.post('/', (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({message: 'No user found with that username'});
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username =dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json({user: dbUserData, message: 'YOU ARE LOGGED IN!!'});
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
